0f4d5372759e838710343f7ca15516656831bf63
weiler1 <dotweilerofficial@gmail.com>
2025-05-21 14:11:18 +0530

    added multiline messages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 async function share(){
     console.log("hi");
-    const twt = document.getElementById('inp').value;
+    var twt = document.getElementById('inp').value;
+    twt = twt.concat('%0ACheck out the Megusta games on https://megustaapp.netlify.app');
     window.open("https://twitter.com/intent/tweet?text=".concat(twt),"_blank");
 }
 window.share = share;
